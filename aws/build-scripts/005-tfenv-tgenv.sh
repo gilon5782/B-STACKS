284088feb04576f8561e37c3b5e1e19cd79b670b
Alon <alon.lavie55@gmail.com>
2022-09-15 16:31:32 +0100

    added build script 005

---
+++ aws/build-scripts/005-tfenv-tgenv.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash 
+# shellcheck source=/dev/null
+set -echo
+
+buil_scripts_dir=$(dirname "$(readlink -f "$0")")
+source "$build_scripts_dir"/common.sh
+
+if [[ ! -d ~/.tfenv ]]; then git clone https://github.com/tfutils/tfenv.git ~/.tfenv; fi
+# this is a public git library, check before use 
+if [[ ! -d ~/.tgenv ]]; then git clone https://github.com/cunymattieu/tgenv.git ~/.tgenv; fi
+
+cp -f .terraform-version ~ && cp -f .terragrunt-version ~
+tfenv install
+tgenv install
